7a4bb13c705ea63009bfaca2d2ed205d2d13403d
Teddy Rendahl <trendahl@slac.stanford.edu>
2017-03-03 22:54:07 -0800

    Adding test framework

---
+++ tests/conftest.py
@@ -0,0 +1,19 @@
+############
+# Standard #
+############
+
+###############
+# Third Party #
+###############
+import pytest
+
+##########
+# Module #
+##########
+
+
+class PseudoMotor:
+
+    def __init__(self, position):
+        self.position = position
+
