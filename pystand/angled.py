b1e4114f4082cec95540576ec021242036127ccb
Teddy Rendahl <trendahl@slac.stanford.edu>
2017-03-05 22:38:21 -0800

    Changed AngledJoint object to return Point object where appropripate and also use one for initialization. This makes it simple to encode the offset information of the joint. The difference between the stand coordinates and joint are now separated as offset / displacement properties

--- pystand/angled.py
+++ pystand/angled.py
@@ -14,7 +14,7 @@ from math import cos, tan, sin
 ##########
 # Module #
 ##########
-
+from .points import Point
 logger = logging.getLogger(__name__)
 
 
@@ -27,12 +27,11 @@ class AngledJoint(object):
     slide : ``ophyd.EpicsMotor``
         Horizontal Motor
 
-    lift  : ``ophyd.EpicsMotor``
+    lift  : ``ophyd.EpicsMotor``, optional
         Vertical Motor
 
-    x0
-
-    y0
+    offset : `.Point`
+        The position of the joint when all motors are at nominal zero
 
     Attributes
     ----------
@@ -41,55 +40,75 @@ class AngledJoint(object):
     """
     alpha =0.261799387
 
-    def __init__(self, slide=None, lift=None,
-                 x0=0., y0=0.0):
+    def __init__(self, slide=None, lift=None, offset=None):
 
-        self.slide = slide
-        self.lift  = lift
-        self.x0    = x0
-        self.y0    = y0
+        self.slide  = slide
+        self.lift   = lift
+        self.offset = offset
+
+
+    @property
+    def displacement(self):
+        """
+        Displacement of the angled motor from the nominal zero
+        """
+        if not self.lift:
+            return Point(self.slide.position, 0., 0.)
+        else:
+            return Point(self.slide.position, self.lift.position, 0.)
 
 
     @property
     def position(self):
         """
-        Position of the two motor axes
+        Position of the ball joint in stand coordinates
         """
-        return (self.slide.position, self.lift.position)
+        return Point(self.joint[0]+self.offset.x,
+                     self.joint[1]+self.offset.y,
+                     self.offset.z)
 
 
     @property
     def joint(self):
         """
-        Position of the ball joint
+        Displacement of the ball joint from nominal zero
         """
-        (slide, lift) = self.position
-        return (lift*cos(self.alpha) + slide,
-                lift*sin(self.alpha))
+        return Point(self.displacement.y*cos(self.alpha) + self.displacement.x,
+                     self.displacement.y*sin(self.alpha),
+                     0.)
 
 
-
-    def invert(self, coordinate):
+    def invert(self, point):
         """
         Invert the matrix to find the neccesary motor positions to put the
-        joint at a specific (x,y) coordinate
+        joint at a specific displacement (x,y) coordinate
 
         Parameters
         -----------
-        coordinate : tuple
+        point : tuple or :class:`.Point`
             The desired x,y coordinates of the joint
 
         Returns
         --------
         position : tuple
             A tuple of the neccesary positions of the form (slide, lift)
+
+        Raises
+        ------
+        ValueError : 
+            If the given position is not possible for the joint
         """
-        return (coordinate[0]-coordinate[1]/tan(self.alpha),
-                coordinate[1]/sin(self.alpha))
+        if not self.lift and point[1] != self.offset.y:
+            raise ValueError("Unable to reach desired position {},"
+                             "because this joint has no lift"
+                             "".format(point[1]))
+
+        return (point[0]-point[1]/tan(self.alpha),
+                point[1]/sin(self.alpha))
 
 
     def __repr__(self):
-        return "AngledJoint at {}".format(self.joint)
+        return "AngledJoint at {!r}".format(self.joint)
 
 
     def __eq__(self, other):
--- tests/test_angled.py
+++ tests/test_angled.py
@@ -11,31 +11,42 @@ import pytest
 ##########
 # Module #
 ##########
-from pystand  import AngledJoint
+from pystand  import AngledJoint, Point
 from conftest import PseudoMotor
 
 
 @pytest.fixture(scope='module')
 def pseudo_angled():
-    angled = AngledJoint(slide = PseudoMotor(5),
-                         lift  = PseudoMotor(10))
+    angled = AngledJoint(slide  = PseudoMotor(5),
+                         lift   = PseudoMotor(10),
+                         offset = Point(1,2,3))
     return angled
 
 def test_joint(pseudo_angled):
     #Test Vertical 
     pseudo_angled.alpha = math.pi/2.
-    assert pytest.approx(pseudo_angled.joint[0]) == 5
-    assert pytest.approx(pseudo_angled.joint[1]) == 10
+    assert pytest.approx(pseudo_angled.joint.x) == 5
+    assert pytest.approx(pseudo_angled.joint.y) == 10
 
     #Test Horizontal
     pseudo_angled.alpha= 0
-    assert pseudo_angled.joint == (15, 0)
+    assert pseudo_angled.joint.x == 15
+    assert pseudo_angled.joint.y == 0
 
 def test_invert(pseudo_angled):
     #Test 45
     pseudo_angled.alpha = math.pi/4.
     assert pytest.approx(pseudo_angled.invert((12.07,7.07))[0]) == pytest.approx(5,0.1)
     assert pytest.approx(pseudo_angled.invert((12.07,7.07))[1]) == pytest.approx(10,0.1)
+
+def test_position(pseudo_angled):
+    pseudo_angled.alpha= 0
+    assert pseudo_angled.position == (16, 2, 3)
+    
+    pseudo_angled.alpha = math.pi/2.
+    assert pseudo_angled.position.x == pytest.approx(6,0.1)
+    assert pseudo_angled.position.y == 12
+    assert pseudo_angled.position.z == 3
 
 def test_cmp():
     p1 = PseudoMotor(5)
