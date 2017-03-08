5e23804e1f94cd54e2aeb7bbae87bce6d2f2d42f
Teddy Rendahl <trendahl@slac.stanford.edu>
2017-03-07 20:59:01 -0800

    Updated tests for separate conejoint angledjoint methods

--- tests/test_angled.py
+++ tests/test_angled.py
@@ -11,42 +11,90 @@ import pytest
 ##########
 # Module #
 ##########
-from detrot  import AngledJoint, Point
+from detrot  import ConeJoint, AngledJoint, Point
 from conftest import PseudoMotor
 
 
 @pytest.fixture(scope='module')
-def pseudo_angled():
+def pseudo_cone():
+    angled = ConeJoint(slide  = PseudoMotor(5),
+                       lift   = PseudoMotor(10),
+                       offset = Point(1,2,3))
+    return angled
+
+@pytest.fixture(scope='module')
+def pseudo_angle():
     angled = AngledJoint(slide  = PseudoMotor(5),
                          lift   = PseudoMotor(10),
                          offset = Point(1,2,3))
     return angled
 
-def test_joint(pseudo_angled):
+
+def test_cone_joint(pseudo_cone):
     #Test Vertical 
-    pseudo_angled.alpha = math.pi/2.
-    assert pytest.approx(pseudo_angled.joint.x) == 5
-    assert pytest.approx(pseudo_angled.joint.y) == 10
+    pseudo_cone.alpha = math.pi/2.
+    assert pytest.approx(pseudo_cone.joint.x) == 5
+    assert pytest.approx(pseudo_cone.joint.y) == 10
 
     #Test Horizontal
-    pseudo_angled.alpha= 0
-    assert pseudo_angled.joint.x == 15
-    assert pseudo_angled.joint.y == 0
+    pseudo_cone.alpha= 0
+    assert pseudo_cone.joint.x == 15
+    assert pseudo_cone.joint.y == 0
 
-def test_invert(pseudo_angled):
+def test_cone_invert(pseudo_cone):
     #Test 45
-    pseudo_angled.alpha = math.pi/4.
-    assert pytest.approx(pseudo_angled.invert((12.07,7.07))[0]) == pytest.approx(5,0.1)
-    assert pytest.approx(pseudo_angled.invert((12.07,7.07))[1]) == pytest.approx(10,0.1)
+    pseudo_cone.alpha = math.pi/4.
+    assert pseudo_cone.invert((13.07,9.07))[0] == pytest.approx(5,0.1)
+    assert pseudo_cone.invert((13.07,9.07))[1] == pytest.approx(10,0.1)
 
-def test_position(pseudo_angled):
-    pseudo_angled.alpha= 0
-    assert pseudo_angled.position == (16, 2, 3)
+def test_angle_joint(pseudo_angle):
+    #Test Vertical
+    pseudo_angle.alpha = math.pi/2.
+    assert pytest.approx(pseudo_angle.joint.x) == 5
+    assert pytest.approx(pseudo_angle.joint.y) == 10
+    assert pytest.approx(pseudo_angle.joint.z) == 0
     
-    pseudo_angled.alpha = math.pi/2.
-    assert pseudo_angled.position.x == pytest.approx(6,0.1)
-    assert pseudo_angled.position.y == 12
-    assert pseudo_angled.position.z == 3
+    #Test Horizontal
+    pseudo_angle.alpha = 0
+    assert pytest.approx(pseudo_angle.joint.x) == 5
+    assert pytest.approx(pseudo_angle.joint.y) == 0
+    assert pytest.approx(pseudo_angle.joint.z) == -10
+
+    #Test no-slide
+    p2 = pseudo_angle.slide
+    pseudo_angle.slide = None
+    assert pytest.approx(pseudo_angle.joint.x) == 0
+    assert pytest.approx(pseudo_angle.joint.y) == 0
+    assert pytest.approx(pseudo_angle.joint.z) == -10
+    pseudo_angle.slide = p2
+
+def test_angle_invert(pseudo_angle):
+    #Test Vertical
+    pseudo_angle.alpha = math.pi/2.
+    assert pseudo_angle.invert((6,12))[0] == pytest.approx(5,0.1)
+    assert pseudo_angle.invert((6,12))[1] == pytest.approx(10,0.1)
+
+    #Test no-slide
+    p2 = pseudo_angle.slide
+    pseudo_angle.slide = None
+    assert pseudo_angle.invert((6,12)) == pytest.approx(10,0.1)
+    pseudo_angle.slide = p2
+
+def test_position(pseudo_cone):
+    pseudo_cone.alpha= 0
+    assert pseudo_cone.position == (16, 2, 3)
+
+    pseudo_cone.alpha = math.pi/2.
+    assert pseudo_cone.position.x == pytest.approx(6,0.1)
+    assert pseudo_cone.position.y == 12
+    assert pseudo_cone.position.z == 3
+
+def test_displacement(pseudo_angle):
+    assert pseudo_angle.displacement == (5,10)
+    p2 = pseudo_angle.slide
+    pseudo_angle.slide = None
+    assert pseudo_angle.displacement == 10
+    pseudo_angle.slide = p2
 
 def test_cmp():
     p1 = PseudoMotor(5)
