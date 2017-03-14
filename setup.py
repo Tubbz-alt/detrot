2508f3ac020678f163f68ecf974b3c2607514435
Teddy Rendahl <trendahl@slac.stanford.edu>
2017-03-14 15:13:04 -0700

    Added a few more setuptools metadata

--- setup.py
+++ setup.py
@@ -2,12 +2,16 @@ import versioneer
 from setuptools import (setup, find_packages)
 
 
-setup(name     = 'detrot',
-      version  = versioneer.get_version(),
-      cmdclass = versioneer.get_cmdclass(),
-      author   = 'SLAC National Accelerator Laboratory',
+setup(name= 'detrot',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      author='SLAC National Accelerator Laboratory',
 
-      packages    = find_packages(),
-      description = 'Python framework for manipulating the CXI detector stands'
-
+      packages=find_packages(),
+      description='Python framework for manipulating the CXI detector stands',
+      long_description=with open('README.rst') as f: f.read(),
+      classifiers=[
+          'Programming Language :: Python :: 3.4',
+          'Topic :: Robotics, EPICS'
+      ],
     )
