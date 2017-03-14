12702b7c49f1e1a4c6e77efaa92757580bc1c9a4
Teddy Rendahl <trendahl@slac.stanford.edu>
2017-03-13 22:15:24 -0700

    DOC: Added module and Point class docstrings

--- detrot/points.py
+++ detrot/points.py
@@ -1,5 +1,22 @@
 """
-Utilities for pystand
+The ``detrot`` module takes advantage of three different reference frames to
+view the Detector stand, the rest frame, the stand frame and the room frame.
+The most intuitive starting point is the rest frame, this ignores any rotation
+and translation of the stand, with the y axis perpindicular to the detector
+base and all joints pointing vertically upwards. From here we can translate to
+the frame of the surrounding stand by looking at the position of the cone joint
+i.e the apex of the three joints that manipulate the stand. We use this as the
+center of rotation for all our rigid body calculations. After accounting for
+the x,y displacement of the cone, we can then do the more complex calculation
+into the room frame. This takes into account the pitch, yaw and roll of the stand
+and the fact that the rest frame axes are not colinear with the surrounding
+room.
+
+Transforming through these different frames is done through the
+:class:`.StandPoint`. Simply enter a point in the rest frame using the cone
+joint as the origin, then check either the :attr:`.stand_coordinates` or
+:attr:`.room_coordinates` to see how the point moves as the angles of the stand
+change.
 """
 ############
 # Standard #
@@ -21,6 +38,33 @@ logger = logging.getLogger(__name__)
 class Point(namedtuple('PointBase', ['x','y','z'])):
     """
     Generic Point Class
+
+    This is a reimplementation of the ``collections.namedtuple`` class. This
+    allows both index and attribute based access to the position variables.
+
+    Parameters
+    ----------
+    x : float
+
+    y : float
+
+    z : float
+
+    Example
+    -------
+    .. ipython:: python
+
+        from detrot import Point
+
+        pnt = Point(1,2,3)
+
+        print(pnt.x)
+
+        pnt.y == 2
+
+        pnt[2] == 3
+
+        pnt == (1,2,3)
     """
     def __repr__(self):
         return "Point (x,y,z -> {},{},{})".format(self.x,
