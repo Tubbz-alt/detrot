ca97203ca9557404dd8a8d238573d39a3919b256
Teddy Rendahl <trendahl@slac.stanford.edu>
2017-03-14 15:42:18 -0700

    Typo in description

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(name= 'detrot',
 
       packages=find_packages(),
       description='Python framework for manipulating the CXI detector stands',
-      long_description=with open('README.rst') as f: f.read(),
       classifiers=[
           'Programming Language :: Python :: 3.4',
           'Topic :: Robotics, EPICS'
